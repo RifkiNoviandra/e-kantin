89802862b2da79b3383c74aa379901318e2522ad
RifkiNoviandra <rnoviandra3@gmail.com>
2022-07-05 14:44:34 +0700

    updaate

--- app/Http/Controllers/Client/Store/transactionController.php
+++ app/Http/Controllers/Client/Store/transactionController.php
@@ -84,8 +84,8 @@ class transactionController extends Controller
     }
 
     function getTransactionByIdAndStore(Request $request , $id , $store_id){
-        $data = Transaction::with([ 'user' ,'detail.menu' => function($query) use($store_id) {
-            return $query->where('store_id' , $store_id);
+        $data = Transaction::with([ 'user' ,'detail' => function($query) use($store_id) {
+            return $query->with('menu')->where('store_id' , $store_id);
         }])->where('id', $id)->first();
 
         return response([       
