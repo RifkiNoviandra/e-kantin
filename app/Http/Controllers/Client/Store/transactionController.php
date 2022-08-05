8d24b3e31ae40e84cfe97da82dec1f0ad8f1dab2
RifkiNoviandra <rnoviandra3@gmail.com>
2022-08-05 09:59:04 +0700

    update

--- app/Http/Controllers/Client/Store/transactionController.php
+++ app/Http/Controllers/Client/Store/transactionController.php
@@ -163,13 +163,13 @@ class transactionController extends Controller
     function listTransactionDone(Request $request)
     {
 
-        $data = Transaction::with(['detail','user'])->where('status', '1')->where('pickup_date', 'LIKE' ,'%2022-08%')->get();
+        $data = Transaction::with(['detail','user'])->where('status', '1')->where('pickup_date', 'LIKE' ,'%'.date('Y-m-d').'%')->get();
 
         if (isset($request->parameter)) {
             $search = strtoupper($request->parameter);
             $data = Transaction::with(['detail', 'user' => function ($query) use ($search) {
                 return $query->where('name', 'LIKE', '%' . $search . '%');
-            }])->where('status', '1')->where('pickup_date', '%' . date('Y-m-d') . '%')->get();
+            }])->where('status', '1')->where('pickup_date', 'LIKE' ,'%'.date('Y-m-d').'%')->get();
 
             return response([
                 $data
